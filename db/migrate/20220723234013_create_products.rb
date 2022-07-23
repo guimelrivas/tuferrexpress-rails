b5046d9ca982ccc36faa4574fb4fb9e576fbf709
guimelrivas <miguel.rivas@alphacorp.cl>
2022-07-23 19:48:09 -0400

    agarrando scafold

---
+++ db/migrate/20220723234013_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :reference
+      t.text :description
+      t.integer :quantity
+      t.decimal :price
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_234013) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "reference"
+    t.text "description"
+    t.integer "quantity"
+    t.decimal "price"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
